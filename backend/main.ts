6d0e7a8a89b06a57780fb44d91312e0401c59479
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2026-04-07 17:27:49 +0200

    refuse boot if temp replays exist

--- backend/main.ts
+++ backend/main.ts
@@ -4,6 +4,21 @@ import { createServer as createViteServer, type ViteDevServer } from "vite";
 import { appRouter } from "./router";
 import "./startgg-import/sapfpresse-updater";
 
+/**
+ * we're checking for temp replay files here and crashing early
+ * so that errors are avoided later with the replay exporter when it might be a lot more annoying to deal with or maybe even when data is already lost because a replay file is discarded or there's some other conflict.
+ */
+
+const temporaryReplayFileGlob = new Bun.Glob("Station_*_temp.slp");
+
+for await (const file of temporaryReplayFileGlob.scan({
+	cwd: "./replays",
+})) {
+	throw new Error(
+		`[Main] Error: Found temporary replay file that should not exist: ${file}. Please investigate and remove these files. Aborting server startup.`,
+	);
+}
+
 const PORT =
 	Bun.env.PORT === undefined ? 3000 : Number.parseInt(Bun.env.PORT, 10);
 
